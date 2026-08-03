8b9988672dc84b7f282367a458b37092b1e84bf0
KolbasoffPavel <koolbasoff.pavel@gmail.com>
2026-08-03 18:38:43 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'state/app_state.dart';
+import 'theme/app_colors.dart';
+import 'theme/app_theme.dart';
+import 'theme/theme_controller.dart';
+import 'screens/home_shell.dart';
+
+void main() {
+    runApp(const PerimetrApp());
+}
+
+class PerimetrApp extends StatelessWidget {
+    const PerimetrApp({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+          return MultiProvider(
+                  providers: [
+                            ChangeNotifierProvider(create: (_) => AppState()),
+                            ChangeNotifierProvider(create: (_) => ThemeController()),
+                          ],
+                  child: Consumer<ThemeController>(
+                            builder: (context, themeController, _) {
+                                        return MaterialApp(
+                                                      title: 'PERIMETR Intelligent Core',
+                                                      debugShowCheckedModeBanner: false,
+                                                      themeMode: themeController.mode,
+                                                      theme: buildLightTheme(),
+                                                      darkTheme: buildDarkTheme(),
+                                                      builder: (context, child) {
+                                                                      final brightness = MediaQuery.platformBrightnessOf(context);
+                                                                      final resolvedBrightness = switch (themeController.mode) {
+                                                                                          ThemeMode.light => Brightness.light,
+                                                                                          ThemeMode.dark => Brightness.dark,
+                                                                                          ThemeMode.system => brightness,
+                                                                      };
+                                                                      final palette = resolvedBrightness == Brightness.dark ? AppPalette.dark : AppPalette.light;
+                                                                      return CupertinoTheme(
+                                                                                        data: buildCupertinoTheme(palette, resolvedBrightness),
+                                                                                        child: child!,
+                                                                                      );
+                                                      },
+                                                      home: Consumer<AppState>(
+                                                                      builder: (context, appState, _) {
+                                                                                        if (!appState.loaded) {
+                                                                                                            return const Scaffold(body: Center(child: CircularProgressIndicator()));
+                                                                                        }
+                                                                                        return const HomeShell();
+                                                                      },
+                                                                    ),
+                                                    );
+                            },
+                          ),
+                );
+    }
+}
